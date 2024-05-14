b49d7b3156f455148d0364e248e1292bd9772d1a
paul90317 <paul2309825@gmail.com>
2024-05-14 04:40:06 +0000

    Get public IP from api.ipify.org

--- src/312552008/http_server.cpp
+++ src/312552008/http_server.cpp
@@ -11,7 +11,10 @@ using boost::asio::ip::tcp;
 class session : public std::enable_shared_from_this<session>
 {
 public:
-    session(tcp::socket socket) : socket_(std::move(socket)) {}
+    session(tcp::socket socket, std::string server_addr)
+        : socket_(std::move(socket)), SERVER_ADDR(server_addr)
+    {
+    }
 
     void start() { read_request(); }
 
@@ -62,7 +65,7 @@ private:
         QUERY_STRING = query_string;
         SERVER_PROTOCOL = server_protocol;
         HTTP_HOST = headers["Host"];
-        SERVER_ADDR = "0.0.0.0";
+        // SERVER_ADDR = "0.0.0.0";
         SERVER_PORT = std::to_string(socket_.local_endpoint().port());
         REMOTE_ADDR = socket_.remote_endpoint().address().to_string();
         REMOTE_PORT = std::to_string(socket_.remote_endpoint().port());
@@ -110,9 +113,10 @@ private:
             });
     }
     tcp::socket socket_;
+    std::string SERVER_PORT;
     std::string request;
     std::string REQUEST_METHOD, REQUEST_URI, QUERY_STRING, SERVER_PROTOCOL,
-        HTTP_HOST, SERVER_ADDR, SERVER_PORT, REMOTE_ADDR, REMOTE_PORT;
+        HTTP_HOST, SERVER_ADDR, REMOTE_ADDR, REMOTE_PORT;
 };
 
 class server
@@ -121,6 +125,69 @@ public:
     server(boost::asio::io_context &io_context, short port)
         : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
     {
+        boost::system::error_code error = boost::asio::error::host_not_found;
+        boost::asio::ip::tcp::socket socket(io_context);
+
+        try {
+            // Fetch the public IP address
+            boost::asio::ip::tcp::resolver resolver(io_context);
+            boost::asio::ip::tcp::resolver::query query("api.ipify.org",
+                                                        "http");
+            boost::asio::ip::tcp::resolver::iterator endpoint_iterator =
+                resolver.resolve(query);
+            boost::asio::ip::tcp::resolver::iterator end;
+
+            while (error && endpoint_iterator != end) {
+                socket.close();
+                socket.connect(*endpoint_iterator++, error);
+            }
+        } catch (...) {
+        }
+
+
+        if (!error) {
+            boost::asio::streambuf request;
+            std::ostream request_stream(&request);
+            request_stream << "GET / HTTP/1.1\r\n";
+            request_stream << "Host: api.ipify.org\r\n";
+            request_stream << "Accept: */*\r\n";
+            request_stream << "Connection: close\r\n\r\n";
+
+            boost::asio::write(socket, request);
+
+            std::string response;
+            boost::asio::read_until(
+                socket, boost::asio::dynamic_buffer(response), "\0");
+
+            std::stringstream response_stream(response);
+
+            std::string http_version;
+            response_stream >> http_version;
+            unsigned int status_code;
+            response_stream >> status_code;
+            std::string status_message;
+            std::getline(response_stream, status_message);
+
+            if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
+                std::cout << "Invalid response\n";
+                return;
+            }
+
+            if (status_code != 200) {
+                std::cout << "Response returned with status code "
+                          << status_code << "\n";
+                return;
+            }
+
+            std::string header;
+            while (std::getline(response_stream, header) && header != "\r") {
+            }
+
+            std::getline(response_stream, SERVER_ADDR);
+        } else {
+            std::cerr << "Error: " << error.message() << std::endl;
+        }
+
         do_accept();
     }
 
@@ -130,7 +197,8 @@ private:
         acceptor_.async_accept(
             [this](boost::system::error_code ec, tcp::socket socket) {
                 if (!ec) {
-                    std::make_shared<session>(std::move(socket))->start();
+                    std::make_shared<session>(std::move(socket), SERVER_ADDR)
+                        ->start();
                 }
 
                 do_accept();
@@ -138,6 +206,7 @@ private:
     }
 
     tcp::acceptor acceptor_;
+    std::string SERVER_ADDR;
 };
 
 int main(int argc, char *argv[])
