9a65acffef17264ab48e5d2aebd6f3de0bef3a4e
paul <paul2309825@gmail.com>
2024-05-15 02:22:02 +0800

    Finish cig_server.exe but some bugs
    The html output will be sent back to golden.

---
+++ src/xbwu90317/cgi_server.cpp
@@ -0,0 +1,200 @@
+#include <boost/algorithm/string.hpp>
+#include <boost/asio.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+#include "console_run.hpp"
+
+using boost::asio::ip::tcp;
+
+boost::asio::io_context io_context;
+
+class session : public std::enable_shared_from_this<session>
+{
+public:
+    session(tcp::socket socket, std::string server_addr)
+        : socket_(std::move(socket)), SERVER_ADDR(server_addr)
+    {
+    }
+
+    void start() { read_request(); }
+
+private:
+    void read_request()
+    {
+        auto self(shared_from_this());
+        // Read data until newline (HTTP request headers usually end with a
+        // blank line)
+        boost::asio::async_read_until(
+            socket_, boost::asio::dynamic_buffer(request), "\r\n\r\n",
+            [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec)
+                    handle_request();
+            });
+    }
+    void handle_request()
+    {
+        auto self(shared_from_this());
+        // Parse request data
+        std::stringstream ss(request);
+        std::string request_method, request_uri, query_string, server_protocol,
+            http_host;
+
+        // Parse request line
+        ss >> request_method >> request_uri >> server_protocol;
+        std::string::size_type pos = request_uri.find('?');
+        if (pos != std::string::npos) {
+            query_string = request_uri.substr(pos + 1);
+            request_uri = request_uri.substr(0, pos);
+        }
+
+        // Parse HTTP headers
+        std::string line;
+        std::unordered_map<std::string, std::string> headers;
+        while (std::getline(ss, line) && !line.empty()) {
+            std::istringstream iss(line);
+            std::string header_name, header_value;
+            std::getline(iss, header_name, ':');
+            std::getline(iss, header_value);
+            boost::algorithm::trim(header_value);
+            headers[header_name] = header_value;
+        }
+
+        // Set environment variables
+        REQUEST_METHOD = request_method;
+        REQUEST_URI = request_uri;
+        QUERY_STRING = query_string;
+        SERVER_PROTOCOL = server_protocol;
+        HTTP_HOST = headers["Host"];
+        // SERVER_ADDR = "0.0.0.0";
+        SERVER_PORT = std::to_string(socket_.local_endpoint().port());
+        REMOTE_ADDR = socket_.remote_endpoint().address().to_string();
+        REMOTE_PORT = std::to_string(socket_.remote_endpoint().port());
+
+        // Write HTTP response header
+        boost::asio::async_write(
+            socket_, boost::asio::buffer(std::string("HTTP/1.1 200 OK\r\n")),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                if (ec)
+                    return;
+                console_run(socket_, QUERY_STRING);
+            });
+    }
+    tcp::socket socket_;
+    std::string SERVER_PORT;
+    std::string request;
+    std::string REQUEST_METHOD, REQUEST_URI, QUERY_STRING, SERVER_PROTOCOL,
+        HTTP_HOST, SERVER_ADDR, REMOTE_ADDR, REMOTE_PORT;
+};
+
+class server
+{
+public:
+    server(short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+    {
+        boost::system::error_code error = boost::asio::error::host_not_found;
+        boost::asio::ip::tcp::socket socket(io_context);
+
+        try {
+            // Fetch the public IP address
+            boost::asio::ip::tcp::resolver resolver(io_context);
+            boost::asio::ip::tcp::resolver::query query("api.ipify.org",
+                                                        "http");
+            boost::asio::ip::tcp::resolver::iterator endpoint_iterator =
+                resolver.resolve(query);
+            boost::asio::ip::tcp::resolver::iterator end;
+
+            while (error && endpoint_iterator != end) {
+                socket.close();
+                socket.connect(*endpoint_iterator++, error);
+            }
+        } catch (...) {
+        }
+
+
+        if (!error) {
+            boost::asio::streambuf request;
+            std::ostream request_stream(&request);
+            request_stream << "GET / HTTP/1.1\r\n";
+            request_stream << "Host: api.ipify.org\r\n";
+            request_stream << "Accept: */*\r\n";
+            request_stream << "Connection: close\r\n\r\n";
+
+            boost::asio::write(socket, request);
+
+            std::string response;
+            boost::asio::read_until(
+                socket, boost::asio::dynamic_buffer(response), "\0");
+
+            std::stringstream response_stream(response);
+
+            std::string http_version;
+            response_stream >> http_version;
+            unsigned int status_code;
+            response_stream >> status_code;
+            std::string status_message;
+            std::getline(response_stream, status_message);
+
+            if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
+                std::cout << "Invalid response\n";
+                return;
+            }
+
+            if (status_code != 200) {
+                std::cout << "Response returned with status code "
+                          << status_code << "\n";
+                return;
+            }
+
+            std::string header;
+            while (std::getline(response_stream, header) && header != "\r") {
+            }
+
+            std::getline(response_stream, SERVER_ADDR);
+        } else {
+            std::cerr << "Error: " << error.message() << std::endl;
+        }
+
+        do_accept();
+    }
+
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec) {
+                    std::make_shared<session>(std::move(socket), SERVER_ADDR)
+                        ->start();
+                }
+
+                do_accept();
+            });
+    }
+
+    tcp::acceptor acceptor_;
+    std::string SERVER_ADDR;
+};
+
+int main(int argc, char *argv[])
+{
+    try {
+        if (argc != 2) {
+            std::cerr << "Usage: http_server <port>\n";
+            return 1;
+        }
+
+        server s(std::atoi(argv[1]));
+
+        io_context.run();
+    } catch (std::exception &e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
---
+++ src/xbwu90317/console_run.hpp
@@ -0,0 +1,211 @@
+#include <unistd.h>  // for write
+#include <boost/algorithm/string.hpp>
+#include <boost/asio.hpp>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+
+using boost::asio::ip::tcp;
+
+extern boost::asio::io_context io_context;
+
+void output_connection(tcp::socket &socket_, std::string is, std::string hi, std::string pi)
+{
+    boost::asio::streambuf buf;
+    std::ostream bout(&buf);
+    bout << "<script>document.getElementById('connection').innerHTML += "
+                 "'<th scope=\"col\">" +
+                     hi + ":" + pi + "</th>';</script>";
+    bout << "<script>document.getElementById('output').innerHTML += "
+                 "'<td><pre id=\"s" +
+                     is + "\" class=\"mb-0\"></pre></td>';</script>";
+    boost::asio::write(socket_, buf);
+}
+
+void replace_all(std::string &line)
+{
+    boost::replace_all(line, "&", "&amp;");
+    boost::replace_all(line, "\"", "&quot;");
+    boost::replace_all(line, "\'", "&apos;");
+    boost::replace_all(line, "<", "&lt;");
+    boost::replace_all(line, ">", "&gt;");
+    boost::replace_all(line, "\r", "");
+    boost::replace_all(line, "\n", "&NewLine;");
+}
+
+void output_shell(tcp::socket &socket_, std::string is, std::string shell)
+{
+    replace_all(shell);
+    boost::asio::streambuf buf;
+    std::ostream bout(&buf);
+    bout << "<script>document.getElementById('s" + is +
+                     "').innerHTML += '" + shell + "';</script>";
+    boost::asio::write(socket_, buf);
+}
+
+void output_command(tcp::socket &socket_, std::string is, std::string command)
+{
+    replace_all(command);
+    boost::asio::streambuf buf;
+    std::ostream bout(&buf);
+    bout << "<script>document.getElementById('s" + is +
+                     "').innerHTML += '<b>" + command +
+                     "</b>&NewLine;';</script>";
+    boost::asio::write(socket_, buf);
+}
+
+const char *body = R"MAIN(
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>NP Project 3 Sample Console</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Code+Pro"
+      rel="stylesheet"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      href="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678068-terminal-512.png"
+    />
+    <style>
+      * {
+        font-family: 'Source Code Pro', monospace;
+        font-size: 1rem !important;
+      }
+      body {
+        background-color: #212529;
+      }
+      pre {
+        color: #cccccc;
+      }
+      b {
+        color: #01b468;
+      }
+    </style>
+  </head>
+  <body>
+    <table class="table table-dark table-bordered">
+      <thead>
+        <tr id="connection">
+        </tr>
+      </thead>
+      <tbody>
+        <tr id="output">
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
+)MAIN";
+
+class golden_session : public std::enable_shared_from_this<golden_session>
+{
+public:
+    golden_session(tcp::socket socket, std::fstream file, std::string is)
+        : socket_(std::move(socket)), file_(std::move(file)), is_(is)
+    {
+    }
+
+    void start() { read_response(); }
+
+private:
+    void write_command()
+    {
+        if (!std::getline(file_, line_))
+            return;
+        auto self(shared_from_this());
+        output_command(socket_, is_, line_);
+        boost::asio::async_write(
+            socket_, boost::asio::buffer(line_ + "\n"),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                if (!ec)
+                    read_response();
+            });
+    }
+    void read_response()
+    {
+        auto self(shared_from_this());
+        response_ = "";
+        boost::asio::async_read_until(
+            socket_, boost::asio::dynamic_buffer(response_), '%',
+            [this, self](const boost::system::error_code &error,
+                         size_t bytes_transferred) {
+                if (!error) {
+                    output_shell(socket_, is_, response_);
+                    write_command();  // Continue reading until EOF
+                } else if (error != boost::asio::error::eof) {
+                    std::cerr << "Read error: " << error.message() << std::endl;
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    std::fstream file_;
+    std::string is_;
+    std::string line_;
+    std::string response_;
+};
+
+void execute(std::string host,
+             std::string port,
+             std::string filename,
+             std::string is)
+{
+    // Open file for reading
+    std::fstream file("./test_case/" + filename,
+                      std::ios_base::openmode::_S_in);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Create socket
+    tcp::socket socket(io_context);
+
+    // Resolve endpoint
+    tcp::resolver resolver(io_context);
+    auto endpoints = resolver.resolve(host, port);
+
+    // Connect to server
+    boost::asio::connect(socket, endpoints);
+    std::make_shared<golden_session>(std::move(socket), std::move(file), is)->start();
+}
+
+void console_run(tcp::socket &socket_, std::string QUERY_STRING)
+{
+    // Print HTTP headers
+    boost::asio::streambuf buf;
+    std::ostream bout(&buf);
+    bout << "Content-Type: text/html\r\n\r\n";
+    bout << body;
+    boost::asio::write(socket_, buf);
+    
+
+    // parse query
+    char *v, *qstr = &QUERY_STRING[0];
+    std::unordered_map<std::string, std::string> querys;
+    while ((v = strtok_r(qstr, "&", &qstr)) != 0) {
+        char *k = strtok_r(v, "=", &v);
+        querys[k] = v;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        std::string is = std::to_string(i);
+        std::string hi = querys["h" + is], pi = querys["p" + is],
+                    fi = querys["f" + is];
+        if (hi.size() && pi.size() && fi.size()) {
+            execute(hi, pi, fi, is);
+            output_connection(socket_, is, hi, pi);
+        }
+    }
+}
